227f23230ac88d8b28a477e920edce8444b020ed
Gururaj1119 <114040066+Gururaj1119@users.noreply.github.com>
2022-10-14 18:58:06 +0530

    changed

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Testing auto trigger of Jenkins job using Webhooks"
-echo "try without grok"
+echo "try without grok proxy"
 echo "Triggering build using poll SCM"
 echo "SUCCESS"
